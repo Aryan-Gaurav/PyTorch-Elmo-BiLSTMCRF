6c7fdd7534c90052befa20a1742e686e011e7fa0
Aryan Gaurav <aryangaurav0504@gmail.com>
2022-04-20 03:26:23 +0530

    Update core.py

--- model/core.py
+++ model/core.py
@@ -41,7 +41,7 @@ def T(a, half=False, cuda=True):
         elif a.dtype in (np.float32, np.float64):
             a = torch.cuda.HalfTensor(a) if half else torch.FloatTensor(a)
         else: raise NotImplementedError(a.dtype)
-    if cuda: a = to_gpu(a, async=True)
+    if cuda: a = to_gpu(a) #, async=True)
     return a
 
 def load_ner_model(m, p, strict=True):
